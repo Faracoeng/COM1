9246e0a93ae4aed0b78c8e6f168817147990c336
Faracoeng <alfmazucheli@gmail.com>
2021-05-12 11:19:24 -0300

    script aprimorado

--- Matlab/Seno- Aula01.m
+++ Matlab/Seno- Aula01.m
@@ -1,12 +1,23 @@
 
-% Para esboçar a onda senoidal necessita de uma base de tempo
 
-% Definindo período de -10 até 10 com passo=0.01
-t= -10:0.01:10;
+% Para esboçar a onda senoidal necessitamos de uma base de tempo
 
+% Definindo período de tempo
+TInicial = input("Insira o tempo inicial do período: ");
+
+TFinal = input("Insira o tempo final do período: ");
+
+passo = input("Insira o passo em que o período avança, utilize (.) no caso de número decimal: ");
+
+t= TInicial:passo:TFinal;
+
+% Definindo amplitude
+a=input("Insira o valor da amplitude: ");
+% Definindo frequência de 1Khz
+f=input("Insira o valor da frequência: ");
 % função senoidal
 
-y = sin(2*pi*t);
+y = a*sin(2*pi*t*f);
 
 % Mostrando o gráfico dos valores de y em função de t
 plot(t,y)
@@ -16,3 +27,6 @@ xlabel("Tempo")
 ylabel("Amplitude")
 
 title("Função seno no domínio do tempo")
+
+% Exibir as grades de fundo
+grid on
